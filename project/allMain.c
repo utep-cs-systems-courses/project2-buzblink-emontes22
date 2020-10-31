9493b60c2828c339cc0a27cc303e7d189d9f7738
Ervey Montes <ervey025@gmail.com>
2020-10-31 16:32:17 -0600

    Combined main from all files

---
+++ project/allMain.c
@@ -0,0 +1,18 @@
+#include <msp430.h>
+#include "libTimer.h"
+#include "led.h"
+#include "switches.h"
+#include "buzzer.h"
+
+int main(void){
+
+  configureClocks();                /* setup master oscillator, CPU & peripheral clocks */
+  led_init();
+  enableWDTInterrupts();            /* enable periodic interrupt */
+  switch_init();
+  buzzer_init();
+  
+  buzzer_set_period(1000);          /* start buzzing!!! */
+  
+  or_sr(0x18);                      // CPU off, GIE on
+}
