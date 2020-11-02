c0733d6c95d8d4c0d23b5c73df1776e569b7f380
emontes22 <60415695+emontes22@users.noreply.github.com>
2020-11-01 22:49:28 -0700

    Update wdInterruptHandler.c

--- project/wdInterruptHandler.c
+++ project/wdInterruptHandler.c
@@ -5,7 +5,7 @@ void
 __interrupt_vec(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
   static char blink_count = 0;
   if (++blink_count == 125) {
-    toggle_both_no_sound();
+    
     blink_count = 0;
   }
 }
