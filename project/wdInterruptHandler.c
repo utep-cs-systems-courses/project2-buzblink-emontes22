f093461d5bc8f0d920de9d0b16ef480c00667836
Ervey Montes <ervey025@gmail.com>
2020-11-01 22:09:19 -0700

    interrupt fixed

--- project/wdInterruptHandler.c
+++ project/wdInterruptHandler.c
@@ -5,7 +5,8 @@ void
 __interrupt_vec(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
   static char blink_count = 0;
   if (++blink_count == 125) {
-    //    state_advance();
+    toggle_both_no_sound();
     blink_count = 0;
   }
+  led_update();
 }
